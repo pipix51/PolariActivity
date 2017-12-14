230be3e17c96da8e0ac906cf5e1ce878563d6499
pipix51 <koh.pirong@dhs.sg>
2017-12-14 14:41:43 +0800

    Update test_ftp.py

--- twisted/test/test_ftp.py
+++ twisted/test/test_ftp.py
@@ -991,7 +991,7 @@ class FTPServerPasvDataConnectionTests(FTPServerTestCase):
             "NLST",
             (u'my resum\xe9', (
                 0, 1, filepath.Permissions(0o777), 0, 0, 'user', 'group')),
-            'my resum\xc3\xa9\r\n')
+            'my resum'+chr(195)+chr(169)+'\r\n')
 
 
     def test_NLSTNonASCIIBytes(self):
